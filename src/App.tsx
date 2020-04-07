78d95e0802ba5095b2867c2783dc3b96c72eefc4
Bstijn <baltessen.stijn@gmail.com>
2020-04-07 09:01:50 +0200

    register screen

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import Description from './Description';
 import Header from './Header';
+import Register from "./Register/Login";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Header name="react"/>
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        {/*<Header name="Stijn"/>*/}
+        {/*<Description countBy={3} />*/}
+        <Register />
       </header>
-      <Description countBy={3} />
     </div>
   );
 }
---
+++ src/connectiostring.ts
@@ -0,0 +1,5 @@
+// TODO @Stijn: find best practice
+
+export class connectiostring {
+    public static connectionstring: string = "https://localhost:44327/:";
+}
