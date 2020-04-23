72a789812300d4d928ac6756944190d573fbfa05
Bstijn <baltessen.stijn@gmail.com>
2020-04-23 12:52:15 +0200

    wes-126-localstorage: succesfully implemented

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -4,32 +4,50 @@ import {Nav, Navbar} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {connect} from "react-redux";
 
+import {authenticationState} from "../reducers/authenticationReducer";
+
 const Navigation = (props: any) => {
     let showlogin;
     let register;
     let login;
-    if (props.authentication.isAuthenticated) {
-        showlogin = <Nav.Link href="/">true</Nav.Link>;
-        register = <Nav.Link href="/logout">Logout</Nav.Link>
+    let isAuthenticated : boolean = false;
+    let authstring : string  = localStorage.getItem("auth") ?? "";
+    let authobject : authenticationState = JSON.parse(authstring);
+    isAuthenticated = authobject.isAuthenticated;
+
+    const homepage = () => {
+        props.history.push("/");
+    };
+    const loginpage = () => {
+        props.history.push("/login");
+    };
+    const registerpage = () => {
+        props.history.push("/login");
+    };
+    const logout = () => {
+        var empty : authenticationState = {
+            id: 0, isAuthenticated: false, token: "", username: ""
+        };
+        localStorage.setItem("auth",JSON.stringify(empty));
+        props.history.push("/login");
+    };
+
+    if (isAuthenticated) {
+        showlogin = <Nav.Link>Hello {authobject.username}</Nav.Link>;
+        register = <Nav.Link onClick={logout}>Logout</Nav.Link>
         login = null;
     }
     else{
-        showlogin = <Nav.Link href="/">false</Nav.Link>;
-        login = <Nav.Link href="/login">Login</Nav.Link>
-        register = <Nav.Link href="/register">Register</Nav.Link>
+        login = <Nav.Link onClick={loginpage}>Login</Nav.Link>
+        register = <Nav.Link onClick={registerpage}>Register</Nav.Link>
 
     }
 
-    const home = () => {
-        props.history.push("/");
-    };
-
-    console.log(props.authentication)
     return (
         <div>
             <Navbar bg="light" expand="lg">
                 <Nav className="mr-auto">
-                    <Nav.Link onClick={home}>Home</Nav.Link>
+                    <Nav.Link onClick={homepage}>Home</Nav.Link>
                     {register}
                     {login}
                     {showlogin}
