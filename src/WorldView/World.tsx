30c541f882c80f04a2444c9bda3e0b9e5a57c683
Bstijn <baltessen.stijn@gmail.com>
2020-06-03 12:27:47 +0200

    wes-189: scalable grid

--- src/WorldView/World.tsx
+++ src/WorldView/World.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {CSSProperties, useEffect} from "react";
 import {useParams, withRouter} from "react-router";
 import {connect} from "react-redux";
 import {initGrid} from "../Types/Grid";
@@ -18,42 +18,51 @@ const World = (props: any) => {
     const [cellHtmlBlock, setCellHtmlBlock] = React.useState();
     const [error, setError] = React.useState(<div/>);
 
+    let mouseDown = false;
+
     useEffect(() => {
         initiliaze();
     }, []);
 
     const AddError = async (error: any) => {
-        setError(<Alert variant={"warning"} onClick={() => setError(<div/>)}>{error.message}</Alert>)
+        setError(<Alert  variant={"warning"} onClick={() => setError(<div/>)}>{error.message}</Alert>)
+    };
+
+    const chunkStyle = (posx: number, posy: number) : CSSProperties=>{
+        let something :CSSProperties = {gridArea: ""+posy + " / " + posx + " /"+ " span 1 "+ "/ " + "span 1"} ;
+        return something;
+    };
+
+    const cellStyle = (color : string, x: number ,y: number): CSSProperties =>{
+        let something :CSSProperties = {backgroundColor: color,gridArea: ""+y + " / " + x + " /"+ " span 1 "+ "/ " + "span 1"} ;
+        return something;
     };
     const loadchunk = async (chunk: Chunk, name: string, y: number, x: number): Promise<any> => {
-        let arr = [[<div/>, <div/>, <div/>]
-            , [<div/>, <div/>, <div/>]
-            , [<div/>, <div/>, <div/>]];
+        let arr = [[<div/>, <div/>, <div/>, <div/>, <div/>],[<div/>, <div/>, <div/>, <div/>, <div/>],[<div/>, <div/>, <div/>, <div/>, <div/>],[<div/>, <div/>, <div/>, <div/>, <div/>],[<div/>, <div/>, <div/>, <div/>, <div/>]];
 
-        for (let y = 0; y < 3; y++) {
-            for (let x = 0; x < 3; x++) {
+        for (let y = 0; y < 5; y++) {
+            for (let x = 0; x < 5; x++) {
                 var cell = chunk.cells[y][x];
-                arr[y][x] = <div style={{backgroundColor: cell.color, gridColumn: (x + 1), gridRow: (y + 1)}}
-                                 className={"cell"}></div>
+                arr[y][x] = <div style={cellStyle(cell.color,x+1,y+1)} className={"cell"}></div>
             }
         }
         return <div className={"chunk"} style={
-            {gridColumn: (x + 1), gridRow: (y + 1)}}>
+            chunkStyle(x+1,y+1)}>
             {arr}
         </div>;
 
     };
 
-    const HighestX = (grid : Chunk[][]) : number =>{
-        let highest : number= 0;
-        for(var row in grid){
-            if(row.length > highest){
+    const HighestX = (grid: Chunk[][]): number => {
+        let highest: number = 0;
+        for (var row in grid) {
+            if (row.length > highest) {
                 highest = row.length;
             }
         }
         return highest;
     };
-    const HighestY = (grid : Chunk[][]) : number =>{
+    const HighestY = (grid: Chunk[][]): number => {
         return grid.length;
     };
 
@@ -61,35 +70,54 @@ const World = (props: any) => {
         try {
             let grid = await GetWorldGrid(id);
             setGrid(grid);
-            // Use this code to show grid capabilites
-            // let tempchunk = grid.grid[0][0];
-            // grid.grid = [[tempchunk, tempchunk, tempchunk, tempchunk],
-            //     [tempchunk, tempchunk, tempchunk],
-            //     [tempchunk, tempchunk, tempchunk,tempchunk,tempchunk,tempchunk, tempchunk, tempchunk],
-            //     [tempchunk, tempchunk, tempchunk,tempchunk,tempchunk, tempchunk, tempchunk]
-            //     ];
-            let highestX : number = HighestX(grid.grid);
-            let highestY : number = HighestY(grid.grid);
+            let highestX: number = HighestX(grid.grid);
+            let highestY: number = HighestY(grid.grid);
             let bightml = new Array(highestY);
             for (let y: number = 0; y < grid.grid.length; y++) {
                 bightml[y] = new Array(highestX)
                 let chunkarray: Chunk[] = grid.grid[y];
                 for (let x: number = 0; x < chunkarray.length; x++) {
-                    console.log("X: "+x, " Y: " +y + " chunk:" + grid.grid[y][x]);
+                    console.log("X: " + x, " Y: " + y + " chunk:" + grid.grid[y][x]);
                     bightml[y][x] = await loadchunk(grid.grid[y][x], "chunk pos= [" + y + "," + x + "]", y, x);
                 }
             }
             let something = JSON.stringify(bightml);
             setCellHtmlBlock(bightml);
         } catch (error) {
-                console.log(error);
+            console.log(error);
             AddError(error);
         }
     };
+    let prevX: number;
+    let prevY: number;
+    let afterX, afterY: number;
+    const startDrag = async (event: any) => {
+        prevX = event.clientX;
+        prevY = event.clientY;
+        // while(mouseDown){
+        //     setTimeout(startDrag, 100);
+        // }
+        console.log("X: " + prevX + " Y: " + prevY);
+    };
+
+    const stopDrag = async (event: any) => {
+        afterX = event.clientX;
+        afterY = event.clientY;
+        let deltaY: number = prevY - afterY;
+        let deltaX: number = prevX - afterX;
+        console.log(deltaY);
+        console.log(deltaX);
+        // event.target.scroll(deltaX, deltaY);
+        mouseDown = false;
+    };
+
     return (<div>
         {error}
         <div className={"center"}>
-            <div className={"grid-container"}>
+            <div className={"grid-container"} onMouseDown={(event) => {
+                mouseDown = true;
+                startDrag(event)
+            }} onMouseUp={stopDrag}>
                 {cellHtmlBlock}
             </div>
         </div>
