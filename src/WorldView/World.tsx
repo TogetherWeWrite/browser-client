82639c4ae9de291c9f30ecf528a314fa81e551a3
Bstijn <baltessen.stijn@gmail.com>
2020-06-04 10:33:20 +0200

    wes-189: stable

--- src/WorldView/World.tsx
+++ src/WorldView/World.tsx
@@ -25,39 +25,49 @@ const World = (props: any) => {
     }, []);
 
     const AddError = async (error: any) => {
-        setError(<Alert  variant={"warning"} onClick={() => setError(<div/>)}>{error.message}</Alert>)
+        setError(<Alert variant={"warning"} onClick={() => setError(<div/>)}>{error.message}</Alert>)
     };
 
-    const chunkStyle = (posx: number, posy: number) : CSSProperties=>{
-        let something :CSSProperties = {gridArea: ""+posy + " / " + posx + " /"+ " span 1 "+ "/ " + "span 1"} ;
+    const chunkStyle = (posx: number, posy: number): CSSProperties => {
+        let something: CSSProperties = {gridArea: "" + posy + " / " + posx + " /" + " span 1 " + "/ " + "span 1"};
         return something;
     };
 
-    const cellStyle = (color : string, x: number ,y: number): CSSProperties =>{
-        let something :CSSProperties = {backgroundColor: color,gridArea: ""+y + " / " + x + " /"+ " span 1 "+ "/ " + "span 1"} ;
+    const cellStyle = (color: string, x: number, y: number): CSSProperties => {
+        let something: CSSProperties = {
+            backgroundColor: color,
+            gridArea: "" + y + " / " + x + " /" + " span 1 " + "/ " + "span 1"
+        };
         return something;
     };
     const loadchunk = async (chunk: Chunk, name: string, y: number, x: number): Promise<any> => {
-        let arr = [[<div/>, <div/>, <div/>, <div/>, <div/>],[<div/>, <div/>, <div/>, <div/>, <div/>],[<div/>, <div/>, <div/>, <div/>, <div/>],[<div/>, <div/>, <div/>, <div/>, <div/>],[<div/>, <div/>, <div/>, <div/>, <div/>]];
+        let arr = [[<div/>, <div/>, <div/>, <div/>, <div/>], [<div/>, <div/>, <div/>, <div/>, <div/>], [<div/>, <div/>,
+            <div/>, <div/>, <div/>], [<div/>, <div/>, <div/>, <div/>, <div/>], [<div/>, <div/>, <div/>, <div/>,
+            <div/>]];
 
         for (let y = 0; y < 5; y++) {
             for (let x = 0; x < 5; x++) {
                 var cell = chunk.cells[y][x];
-                arr[y][x] = <div style={cellStyle(cell.color,x+1,y+1)} className={"cell"}></div>
+                arr[y][x] = <div style={cellStyle(cell.color, x + 1, y + 1)} className={"cell"}></div>
             }
         }
         return <div className={"chunk"} style={
-            chunkStyle(x+1,y+1)}>
+            chunkStyle(x + 1, y + 1)}>
             {arr}
         </div>;
 
     };
 
+    const loadPossibleNewChunk = async (y: number, x: number) => {
+        return <div className={"possible-new-chunk"} style={
+            chunkStyle(x + 1, y + 1)}>+</div>
+    };
+
     const HighestX = (grid: Chunk[][]): number => {
         let highest: number = 0;
-        for (var row in grid) {
-            if (row.length > highest) {
-                highest = row.length;
+        for(let x: number = 0; x< grid.length;x++){
+            if(highest<grid[x].length){
+                highest = grid[x].length;
             }
         }
         return highest;
@@ -70,17 +80,25 @@ const World = (props: any) => {
         try {
             let grid = await GetWorldGrid(id);
             setGrid(grid);
+            grid.grid = [
+                [grid.grid[0][0], grid.grid[0][0]]];
             let highestX: number = HighestX(grid.grid);
+            console.log("hx",highestX);
             let highestY: number = HighestY(grid.grid);
-            let bightml = new Array(highestY);
+            let bightml = new Array(highestY + 2); //+2 for above and below
+            bightml[0] = new Array(highestX + 2);
+            bightml[highestY + 1] = new Array(highestX + 2);
+            console.log("l",grid.grid.length);
             for (let y: number = 0; y < grid.grid.length; y++) {
-                bightml[y] = new Array(highestX)
-                let chunkarray: Chunk[] = grid.grid[y];
-                for (let x: number = 0; x < chunkarray.length; x++) {
+                bightml[y + 1] = new Array(highestX + 2); //+2 for left and right extra space
+                for (let x: number = 0; x < grid.grid[y].length; x++) {
                     console.log("X: " + x, " Y: " + y + " chunk:" + grid.grid[y][x]);
-                    bightml[y][x] = await loadchunk(grid.grid[y][x], "chunk pos= [" + y + "," + x + "]", y, x);
+                    bightml[y +1][x+1 ] = await loadchunk(grid.grid[y][x], "chunk pos= [" + y+1 + "," + x+1 + "]", y+1 , x +1);
+                    console.log(0, bightml[0],1, bightml[1],2, bightml[2],3,bightml[3]);
                 }
             }
+
+
             let something = JSON.stringify(bightml);
             setCellHtmlBlock(bightml);
         } catch (error) {
